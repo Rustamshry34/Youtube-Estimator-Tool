99b9ce04804a6cd070b69a700bbbc1cf27629956
Rustam Shiriyev <118140621+Rustamshry34@users.noreply.github.com>
2024-10-07 18:24:41 +0200

    Testing.py

--- Testing.py
+++ Testing.py
@@ -9,7 +9,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from io import StringIO
-import matplotlib.pyplot as plt
 import joblib
 import time
 import requests
