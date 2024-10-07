9104c7e509c416ffd78d74028e601e545ca82eff
Rustam Shiriyev <118140621+Rustamshry34@users.noreply.github.com>
2024-10-07 23:12:19 +0200

    Testing.py

--- Testing.py
+++ Testing.py
@@ -119,7 +119,7 @@ def check_sponsorship_disclaimer(video_url):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     
-    service = Service(executable_path="C:\chromedriver-win64\chromedriver-win64\chromedriver") 
+    service = Service(ChromeDriverManager().install())
 
     driver = webdriver.Chrome(service=service,options=chrome_options)
 
