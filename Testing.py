0e22b7f3c40627ba5052831d42b92b76f776da87
Rustam Shiriyev <118140621+Rustamshry34@users.noreply.github.com>
2024-10-08 20:04:48 +0200

    Testing.py

--- Testing.py
+++ Testing.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import undetected_chromedriver as uc
 import pandas as pd
 from io import StringIO
 import joblib
@@ -113,15 +114,8 @@ def get_video_details(video_id):
 
 def check_sponsorship_disclaimer(video_url):
 
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    driver = uc.Chrome(headless=True)
     
-    service = Service(ChromeDriverManager().install())
-
-    driver = webdriver.Chrome(service=service,options=chrome_options)
-
     driver.get(video_url)
 
     time.sleep(5)
