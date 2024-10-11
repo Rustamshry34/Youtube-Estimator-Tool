cf2b9a7390e686b481a77d1148801ddb97b19813
Rustam Shiriyev <118140621+Rustamshry34@users.noreply.github.com>
2024-10-11 21:14:00 +0200

    Testing.py

--- Testing.py
+++ Testing.py
@@ -117,13 +117,12 @@ def check_sponsorship_disclaimer(video_url):
     #driver = uc.Chrome(headless=True)
     
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
 
-    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    #driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(video_url)
 
     time.sleep(5)
